2c9af171ec81c02e4aa42533c8d2c7a3596099f8
Salvini126 <salvini.matteo@iisgalvanimi.edu.it>
2022-03-07 09:18:38 +0000

    punto2

---
+++ email-salvini/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  Email : string = "";
+  estratto : string = "";
+  Testo : string = "";
+  vett_Email_Oggetto : string[] = [];
+  registrazione(Email : HTMLInputElement, Testo : HTMLInputElement,  Oggetto : HTMLInputElement ){
+    console.log(Email.value, Oggetto.value, Testo.value)
+    this.estratto = (Email.value) + " " + (Oggetto.value)
+  }
+}
